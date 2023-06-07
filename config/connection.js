ed7b000f4e6f1063cdc27593a4b23622ed816b11
Cynthia Mory <cynthia.mory.morales@gmail.com>
2023-06-06 20:30:30 -0400

    had to reaarange my files and folders

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize');
 //ALLOWS US TO USE .ENV FILE TO HIDE SENSITIVE INFO
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       dialectOptions: {
